9d3601de8466562106730e5dd4f226187150970b
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-11-30 14:58:45 +0100

    fix: gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/5hare/",
   plugins: [
     react(),
     tailwindcss(),
